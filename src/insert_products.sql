cfda6f9606c61a23d7166303001ed5556f0e92e8
etragula <misha-petrosyan@mail.ru>
2022-09-18 14:29:20 +0300

    Загрузка файлов на сервер

---
+++ src/insert_products.sql
@@ -0,0 +1,7 @@
+INSERT INTO PRODUCTS VALUES(257462345, 'Смартфон', 'Смартфон');
+INSERT INTO PRODUCTS VALUES(226253322, 'Холодильник', 'Холодильник');
+INSERT INTO PRODUCTS VALUES(368354324, 'Стиральная машинка', 'Стиральная машинка');
+INSERT INTO PRODUCTS VALUES(275235456, 'Электрическая зубная щетка', 'Электрическая зубная щетка');
+INSERT INTO PRODUCTS VALUES(438354624, 'Ноутбук', 'Ноутбук');
+INSERT INTO PRODUCTS VALUES(825265376, 'Микроволновка', 'Микроволновка');
+INSERT INTO PRODUCTS VALUES(755532724, 'Утюг', 'Утюг');
--- src/main/java/org/example/servers/filters/CalculatorFilter.java
+++ src/main/java/org/example/servers/filters/CalculatorFilter.java
@@ -7,27 +7,42 @@ import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
 import java.io.IOException;
+import java.sql.Connection;
 
 @WebFilter(value = "/calculator")
 public class CalculatorFilter extends HttpFilter {
-
     private static final String POST = "POST";
     private static final String ERROR = "error";
     private static final String NUMBER = "Number";
 
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
-        if (POST.equals(req.getMethod())) {
-            var errorMessage = getErrorMessage(req.getParameter(NUMBER));
-            if (errorMessage.isBlank()) {
-                super.doFilter(req, res, chain);
-            }
-            req.setAttribute(ERROR, errorMessage);
-            req.getRequestDispatcher("jsp/calculator.jsp").forward(req, res);
-        } else {
-            super.doFilter(req, res, chain);
+//        if (POST.equals(req.getMethod())) {
+//            var errorMessage = getErrorMessage(req.getParameter(NUMBER));
+//            if (errorMessage.isBlank()) {
+//                super.doFilter(req, res, chain);
+//            }
+//            req.setAttribute(ERROR, errorMessage);
+//            req.getRequestDispatcher("jsp/calculator.jsp").forward(req, res);
+//        } else {
+
+        try {
+            Context initContext = new InitialContext();
+            Context envContext = (Context) initContext.lookup("java:/comp/env");
+            DataSource ds = (DataSource) envContext.lookup("jdbc/My_DB");
+            Connection connection = ds.getConnection();
+        } catch (Exception e) {
+            throw new IllegalArgumentException(e);
         }
+
+
+        super.doFilter(req, res, chain);
+
+//        }
     }
 
     private String getErrorMessage(String number) {
